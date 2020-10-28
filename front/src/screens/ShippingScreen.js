17cd684d223b7f8589031f393381bcfabd21a41f
Meylis <meylis.muhamedov96@gmail.com>
2020-10-28 22:39:37 +0300

    checkout process added

---
+++ front/src/screens/ShippingScreen.js
@@ -0,0 +1,66 @@
+import React, {useState, useEffect} from 'react';
+import { Form, Button  } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import { saveShippingAddress } from '../actions/cartActions';
+
+const ShippingScreen = ({ history }) => {
+    const cart = useSelector(state => state.cart);
+    const { shippingAddress } = cart;
+
+    const [address, setAddress] = useState(shippingAddress.address);
+    const [city, setCity] = useState(shippingAddress.city);
+    const [number, setNumber] = useState(shippingAddress.number);
+
+    const dispatch = useDispatch();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(saveShippingAddress({address, city, number}));
+        history.push('/payment');
+    };
+
+    return (
+        <FormContainer>
+            <h1>Доставка</h1>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId="address">
+                    <Form.Label>Адрес</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        placeholder="Введите адрес"
+                        required
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}>
+                        </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="city">
+                    <Form.Label>Город</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        placeholder="Введите город"
+                        required
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}>
+                        </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="number">
+                    <Form.Label>Номер</Form.Label>
+                    <Form.Control 
+                        type="tel"
+                        placeholder="Введите номер"
+                        required
+                        value={number}
+                        onChange={(e) => setNumber(e.target.value)}>
+                        </Form.Control>
+                </Form.Group>
+
+                <Button type="submit" variant="primary">Продолжить</Button>
+            </Form>
+        </FormContainer>
+    )
+};
+
+export default ShippingScreen;
